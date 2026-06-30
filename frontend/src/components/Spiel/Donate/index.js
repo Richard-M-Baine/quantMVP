194ddcfa1c9335a1ce5a514dc5fdfac911334108
Richard Baine <efdeliveryservice@gmail.com>
2026-06-30 17:19:27 -0400

    got rid of profanity

--- frontend/src/components/Spiel/Donate/index.js
+++ frontend/src/components/Spiel/Donate/index.js
@@ -8,7 +8,7 @@ function DonateComponent() {
     
         <h1>About this project</h1>
        <h3> you cant run a site without money.  </h3>
-        <h3> you cant run a site if you cant get a job due to criminal conviction </h3>
+       
 
     </div>
   );
